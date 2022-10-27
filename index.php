5e28790c22185e943148ecdca932c1236553969e
pelzfx <111582303+pelzfx@users.noreply.github.com>
2022-10-27 15:56:41 +0100

    ds

--- index.php
+++ index.php
@@ -4,49 +4,12 @@ $sessionId = $_POST["sessionId"];
 $serviceCode = $_POST["serviceCode"];
 //This is the first menu screen
 if ( $text == "" ) {
-<<<<<<< HEAD
-$response = "CON Hi welcome, I am your KDoc,  \n";
-=======
-$response = "CON Hi welcome, I'm K-Doc \n";
->>>>>>> c2856bbfdb097db20b4d177bebcc8df5d6a9056a
+$response = "CON Hi welcome, I am KDoc \n";
 $response .= "1. Enter 1 to continue";
-//k-doctor.herokuapp.com/prqwehttps://k-doctor.herokuapp.com/pruio}QWEPRUIO// Menu for a user who selects '1' from the first menu
+}
+// Menu for a user who selects '1' from the first menu
 // Will be brought to this second menu screen
 else if ($text == "1") {
-<<<<<<< HEAD
-  $response = "CON This is K_Doc i am here to answer your Health questions for free Choose Prefered Language \n";
-  $response .= "1. English \n";
-$response .= "3. Pigin \n";
-  
-  }
-  //Menu for a user who selects '1' from the second menu above
-  // Will be brought to this third menu screen
-  else if ($text == "1*1") {
-  $response = "CON How do you identify. choose one? \n";
-  $response .= "1. Male \n";
-  $response .= "2. Female \n"; 
-  $response .= "3. Non-Binary \n";
-  $response .= "4. Enter 0 to cancel\n";
-
-  }
-  else if ($text == "1*1*1") {
-  $response = "CON Got it. How old are you? \n";
-  $response .= "1. 10years - 20years \n";
-  $response .= "2. 21years - 40years \n";
-  $response .= "3. 50+ \n";
-  $response .= "4. Enter 0 to cancel";
-  }
-  else if ($text == "1*1*1*1") {
-  $response = "";
-  }
-  else if ($text == "1*1*1*0") {
-  $response = "Your session has been canceled";
-  }
-  
-  //echo response
-  header('Content-type: text/plain');
-?>
-=======
 $response = "CON Pick a table for reservation below \n";
 $response .= "1. Table for 2 \n";
 $response .= "2. Table for 4 \n";
@@ -128,4 +91,3 @@ $response = "END Your Table reservation for 8 has been canceled";
 header('Content-type: text/plain');
 echo $response
 ?>
->>>>>>> c2856bbfdb097db20b4d177bebcc8df5d6a9056a
